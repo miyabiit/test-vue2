9f358026a48bbdab50830faedacfecf0a30ea31a
miyabiit <yamada.masahiro@gmail.com>
2020-02-23 12:15:32 +0900

    working

--- src/views/SideMenu.vue
+++ src/views/SideMenu.vue
@@ -19,16 +19,14 @@
               <router-link :to="{ name: 'list', params: {Id: 4}}">{{category.name}} - {{category.category_id}} - {{category.position}}</router-link>
             </div>
           </div>
-          <div class="card" v-if="category.position == 2">
+          <div class="card-body" v-if="category.position == 2">
             <div class="card child">
               <div class="card-body" id="heading-1-1">
                 <router-link :to="{ name: 'list', params: {Id: 4}}">{{category.name}} - {{category.category_id}} - {{category.position}}</router-link>
               </div>              
-            </div>
-          </div>
-          <div class="card" v-if="category.position == 3">
-            <div class="card-body" id="heading-1-1">
-              <router-link class="product" :to="{ name: 'list', params: {Id: 4}}">{{category.name}} - {{category.category_id}} - {{category.position}}</router-link>
+              <div class="card" v-if="category.position == 3">
+                  <router-link class="product" :to="{ name: 'list', params: {Id: 4}}">{{category.name}} - {{category.category_id}} - {{category.position}}</router-link>
+              </div>
             </div>
           </div>
         </div>
