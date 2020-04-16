cdeebee72f579c4501dd4a9282e5587960ced937
yamada masahiro <yamada.masahiro@gmail.com>
2020-04-16 21:11:46 +0000

    working breadcrumb

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@
   <global-header/>
   <part-header/>
   <div class="container">
+    <nav aria-label="breadcrumb" class="mt-3 mt-md-0 row">
+      <ol class="breadcrumb col rounded-0">
+        <li class="breadcrumb-item"><a href="index.html">ホーム</a></li>
+        <li class="breadcrumb-item"><a href="category.html">掘削・運搬・解体・林業</a></li>
+        <li class="breadcrumb-item"><a href="product_list.html">掘削機械</a></li>
+        <li class="breadcrumb-item active" aria-current="page">0.007m3（0.3ｔクラス）バックホー 後方小旋回 PC01</li>
+      </ol>
+    </nav>
     <div class="row flex-xl-nowrap">
       <router-view name="mainview" :categorySet="sortCategories"/>
       <div class="order-2 order-md-1 col-12 col-md-3 px-0 bd-sidebar">
